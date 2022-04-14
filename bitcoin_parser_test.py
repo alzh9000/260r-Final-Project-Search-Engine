49d02aa850e25842c5f7ff2f95335d4f62295af5
Albert Zhang <albertzhang9000@gmail.com>
2022-04-14 10:58:16 -0400

    I HATE WINDOWS

--- bitcoin_parser_test.py
+++ bitcoin_parser_test.py
@@ -9,7 +9,11 @@ parent_path = str(Path(__file__).parent)
 
 # Instantiate the Blockchain by giving the path to the directory
 # containing the .blk files created by bitcoind
-blockchain = Blockchain(parent_path + "/.bitcoin/blocks")
+block_path = os.path.abspath(parent_path + "/.bitcoin/blocks")
+print(block_path)
+blockchain = Blockchain(block_path)
+os.listdir("./")
+os.listdir(block_path)
 for block in blockchain.get_unordered_blocks():
     for tx in block.transactions:
         for no, output in enumerate(tx.outputs):

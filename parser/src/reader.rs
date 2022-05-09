541195a5e630158d1768f1dfbe92cebda1cca50e
djsavvy <sraghuvanshi@college.harvard.edu>
2022-05-09 01:49:15 -0400

    remove some dead code

--- parser/src/reader.rs
+++ parser/src/reader.rs
@@ -3,12 +3,8 @@ use nom::{bytes::complete::tag, complete::take, sequence::preceded, IResult};
 pub fn f() -> u32 {
     let file = std::fs::read("/Volumes/SavvyT7Red/BitcoinCore/blocks/blk00000.dat").unwrap();
     let file = file.as_slice();
-    let (_, size) = raw_block_size(file).unwrap();
+    let (_input, size) = raw_block_size(file).unwrap();
     size
-}
-
-fn hex_to_u32(input: &[u8; 4]) -> u32 {
-    u32::from_le_bytes(*input)
 }
 
 pub fn raw_block_size(input: &[u8]) -> IResult<&[u8], u32> {

1d79832dd8835dd3ddf87b0e2214543c8b0741af
djsavvy <sraghuvanshi@college.harvard.edu>
2022-05-11 01:46:08 -0400

    Switch to rusqlite from sqlite

--- parser/src/sqlite.rs
+++ parser/src/sqlite.rs
@@ -1,9 +1,8 @@
-use sqlite::{self, Connection};
-
 use crate::transaction::{Block, InputOutputPair, Transaction};
+use rusqlite::Connection;
 
 pub fn initialize() -> Connection {
-    let connection = sqlite::open("btc-test.db").unwrap();
+    let connection = rusqlite::Connection::open("btc-test.db").unwrap();
 
     connection.execute(
         "
@@ -11,20 +10,11 @@ pub fn initialize() -> Connection {
         CREATE TABLE blocks (block_hash BLOB NOT NULL, version UNSIGNED INT4 NOT NULL, prev_block_id BLOB NOT NULL, merkle_root BLOB NOT NULL, unix_time UNSIGNED INT4 NOT NULL, tx_count UNSIGNED INT4 NOT NULL, height UNSIGNED INT4 NOT NULL);
         CREATE TABLE input_output_pairs (src_tx BLOB NOT NULL, src_index UNSIGNED INT4 NOT NULL, value UNSIGNED INT8 NOT NULL, dest_tx BLOB, dest_index INT4);
         "
-    ,) .unwrap();
+    , []).unwrap();
 
     connection
 
     // TODO: create prepared statements for data insertion and the queries we want to run
-
-    // connection
-    // .iterate("SELECT * FROM blocks WHERE version > 1", |pairs| {
-    // for &(column, value) in pairs.iter() {
-    // println!("{} = {}", column, value.unwrap());
-    // }
-    // true
-    // })
-    // .unwrap();
 }
 
 pub fn insert_tx(conn: &Connection, tx: Transaction) {}

4cc83c61ab4bb8d8ada626b7882faf06b0bf65ab
djsavvy <sraghuvanshi@college.harvard.edu>
2022-05-11 00:57:38 -0400

    Get lifetime working for sqlite functions

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,4 +1,4 @@
-use crate::sqlite::{insert_block, insert_iopair, insert_tx};
+use std::rc::Rc;
 
 mod parser;
 mod sqlite;
@@ -6,7 +6,25 @@ mod transaction;
 
 fn main() {
     println!("Hello, world!");
-    let mut p = parser::Parser::new(insert_tx, insert_block, insert_iopair);
+    let sqlite_driver = sqlite::initialize();
+    let sqlite_driver = Rc::new(sqlite_driver);
+
+    let sqlite_driver1 = sqlite_driver.clone();
+    let itx = move |x| {
+        sqlite::insert_tx(&sqlite_driver1, x);
+    };
+
+    let sqlite_driver2 = sqlite_driver.clone();
+    let btx = move |x| {
+        sqlite::insert_block(&sqlite_driver2, x);
+    };
+
+    let sqlite_driver3 = sqlite_driver.clone();
+    let ptx = move |x| {
+        sqlite::insert_iopair(&sqlite_driver3, x);
+    };
+
+    let mut p = parser::Parser::new(itx, btx, ptx);
+
     p.parse();
-    sqlite::initialize();
 }
--- parser/src/sqlite.rs
+++ parser/src/sqlite.rs
@@ -1,16 +1,11 @@
-use sqlite;
+use sqlite::{self, Connection};
 
 use crate::transaction::{Block, InputOutputPair, Transaction};
 
-pub struct SQLiteDriver {
-    connection: sqlite::Connection,
-}
+pub fn initialize() -> Connection {
+    let connection = sqlite::open("btc-test.db").unwrap();
 
-impl SQLiteDriver {
-    pub fn new() -> SQLiteDriver {
-        let connection = sqlite::open("btc-test.db").unwrap();
-
-        connection.execute(
+    connection.execute(
         "
         CREATE TABLE blocks (block_hash BLOB NOT NULL, version UNSIGNED INT4 NOT NULL, prev_block_id BLOB NOT NULL, merkle_root BLOB NOT NULL, unix_time UNSIGNED INT4 NOT NULL, tx_count UNSIGNED INT4 NOT NULL, height UNSIGNED INT4 NOT NULL);
         CREATE TABLE transactions (id BLOB NOT NULL, version UNSIGNED INT4 NOT NULL, block BLOB NOT NULL, block_height UNSIGNED INT4 NOT NULL, size UNSIGNED INT4 NOT NULL);
@@ -18,23 +13,22 @@ impl SQLiteDriver {
         "
     ,) .unwrap();
 
-        return SQLiteDriver { connection };
+    connection
 
-        // TODO: create prepared statements for data insertion and the queries we want to run
+    // TODO: create prepared statements for data insertion and the queries we want to run
 
-        // connection
-        // .iterate("SELECT * FROM blocks WHERE version > 1", |pairs| {
-        // for &(column, value) in pairs.iter() {
-        // println!("{} = {}", column, value.unwrap());
-        // }
-        // true
-        // })
-        // .unwrap();
-    }
+    // connection
+    // .iterate("SELECT * FROM blocks WHERE version > 1", |pairs| {
+    // for &(column, value) in pairs.iter() {
+    // println!("{} = {}", column, value.unwrap());
+    // }
+    // true
+    // })
+    // .unwrap();
 }
 
-pub fn insert_tx(tx: Transaction) {}
+pub fn insert_tx(conn: &Connection, tx: Transaction) {}
 
-pub fn insert_block(b: Block) {}
+pub fn insert_block(conn: &Connection, b: Block) {}
 
-pub fn insert_iopair(iopair: InputOutputPair) {}
+pub fn insert_iopair(conn: &Connection, iopair: InputOutputPair) {}

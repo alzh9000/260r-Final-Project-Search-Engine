d28da514161eacb062b9dc4008872e56952b4235
djsavvy <sraghuvanshi@college.harvard.edu>
2022-05-10 23:44:14 -0400

    Update main.rs and sqlite.rs to match new parser

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,10 +1,12 @@
+use crate::sqlite::{insert_block, insert_iopair, insert_tx};
+
 mod parser;
 mod sqlite;
 mod transaction;
 
 fn main() {
     println!("Hello, world!");
-    let mut p = parser::Parser::new();
-    // p.parse();
+    let mut p = parser::Parser::new(insert_tx, insert_block, insert_iopair);
+    p.parse();
     sqlite::initialize();
 }

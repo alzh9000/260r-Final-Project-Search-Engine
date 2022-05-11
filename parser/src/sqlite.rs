286ed751e80cc9a842a85ed79fca5651c607b7ae
djsavvy <sraghuvanshi@college.harvard.edu>
2022-05-10 23:44:14 -0400

    Update main.rs and sqlite.rs to match new parser

--- parser/src/sqlite.rs
+++ parser/src/sqlite.rs
@@ -1,23 +1,31 @@
 use sqlite;
 
+use crate::transaction::{Block, InputOutputPair, Transaction};
+
 pub fn initialize() -> () {
     let connection = sqlite::open("/Volumes/SavvyT7Red/Sqlite/btc.db").unwrap();
 
-    connection
-        .execute(
-            "
-        CREATE TABLE blocks (block_hash BLOB, version UNSIGNED INT4);
-        INSERT INTO blocks VALUES ('sdfs', 33);
-        ",
-        )
-        .unwrap();
+    connection.execute(
+        "
+        CREATE TABLE blocks (block_hash BLOB NOT NULL, version UNSIGNED INT4 NOT NULL, prev_block_id BLOB NOT NULL, merkle_root BLOB NOT NULL, unix_time UNSIGNED INT4 NOT NULL, tx_count UNSIGNED INT4 NOT NULL, height UNSIGNED INT4 NOT NULL);
+        CREATE TABLE transactions (tx_hash BLOB, version UNSIGNED INT4, block BLOB, block_height UNSIGNED INT4, size UNSIGNED INT4);
+        "
+    ,) .unwrap();
 
-    connection
-        .iterate("SELECT * FROM blocks WHERE version > 30", |pairs| {
-            for &(column, value) in pairs.iter() {
-                println!("{} = {}", column, value.unwrap());
-            }
-            true
-        })
-        .unwrap();
+    // TODO: create prepared statements for data insertion and the queries we want to run
+
+    // connection
+    // .iterate("SELECT * FROM blocks WHERE version > 1", |pairs| {
+    // for &(column, value) in pairs.iter() {
+    // println!("{} = {}", column, value.unwrap());
+    // }
+    // true
+    // })
+    // .unwrap();
 }
+
+pub fn insert_tx(tx: Transaction) {}
+
+pub fn insert_block(b: Block) {}
+
+pub fn insert_iopair(iopair: InputOutputPair) {}

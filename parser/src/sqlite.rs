1ddda1a2a83d1f5c78c6c7b4e03e06d58e54aa9e
djsavvy <sraghuvanshi@college.harvard.edu>
2022-05-11 00:37:42 -0400

    Add more detail to sqlite schema

--- parser/src/sqlite.rs
+++ parser/src/sqlite.rs
@@ -2,26 +2,35 @@ use sqlite;
 
 use crate::transaction::{Block, InputOutputPair, Transaction};
 
-pub fn initialize() -> () {
-    let connection = sqlite::open("/Volumes/SavvyT7Red/Sqlite/btc.db").unwrap();
+pub struct SQLiteDriver {
+    connection: sqlite::Connection,
+}
 
-    connection.execute(
+impl SQLiteDriver {
+    pub fn new() -> SQLiteDriver {
+        let connection = sqlite::open("btc-test.db").unwrap();
+
+        connection.execute(
         "
         CREATE TABLE blocks (block_hash BLOB NOT NULL, version UNSIGNED INT4 NOT NULL, prev_block_id BLOB NOT NULL, merkle_root BLOB NOT NULL, unix_time UNSIGNED INT4 NOT NULL, tx_count UNSIGNED INT4 NOT NULL, height UNSIGNED INT4 NOT NULL);
-        CREATE TABLE transactions (tx_hash BLOB, version UNSIGNED INT4, block BLOB, block_height UNSIGNED INT4, size UNSIGNED INT4);
+        CREATE TABLE transactions (id BLOB NOT NULL, version UNSIGNED INT4 NOT NULL, block BLOB NOT NULL, block_height UNSIGNED INT4 NOT NULL, size UNSIGNED INT4 NOT NULL);
+        CREATE TABLE input_output_pairs (src_tx BLOB NOT NULL, src_index UNSIGNED INT4 NOT NULL, value UNSIGNED INT8 NOT NULL, dest_tx BLOB, dest_index INT4);
         "
     ,) .unwrap();
 
-    // TODO: create prepared statements for data insertion and the queries we want to run
+        return SQLiteDriver { connection };
 
-    // connection
-    // .iterate("SELECT * FROM blocks WHERE version > 1", |pairs| {
-    // for &(column, value) in pairs.iter() {
-    // println!("{} = {}", column, value.unwrap());
-    // }
-    // true
-    // })
-    // .unwrap();
+        // TODO: create prepared statements for data insertion and the queries we want to run
+
+        // connection
+        // .iterate("SELECT * FROM blocks WHERE version > 1", |pairs| {
+        // for &(column, value) in pairs.iter() {
+        // println!("{} = {}", column, value.unwrap());
+        // }
+        // true
+        // })
+        // .unwrap();
+    }
 }
 
 pub fn insert_tx(tx: Transaction) {}

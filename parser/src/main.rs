a7c6ca4c1c5fd1b1cd10da473d80b18c488aee42
djsavvy <sraghuvanshi@college.harvard.edu>
2022-05-12 18:49:41 -0400

    Implement reader for our custom format

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,4 +1,4 @@
-use crate::custom_format::CustomWriter;
+use crate::custom_format::{read_custom_formats, CustomWriter};
 
 mod custom_format;
 mod output_writer;
@@ -10,12 +10,17 @@ fn main() {
     println!("Hello, world!");
 
     // Change this type to write output in a different format (e.g. SQLiteDriver, or CustomWriter)
-    let mut custom_drainer = CustomWriter::new();
+    // let mut custom_drainer = CustomWriter::new();
 
     // let sqlite_connection = rusqlite::Connection::open("btc-test.db").unwrap();
     // let mut sqlite_drainer = sqlite::SQLiteDriver::new(&sqlite_connection);
 
-    let mut p = parser::Parser::new(&mut custom_drainer);
+    // let mut p = parser::Parser::new(&mut custom_drainer);
 
-    p.parse(1);
+    // p.parse(1);
+
+    let (txs, blocks, iopairs) = read_custom_formats();
+
+    println!("{} {} {}", txs.len(), blocks.len(), iopairs.len());
+    println!("{:#?} {:#?} {:#?}", txs[0], blocks[0], iopairs[0]);
 }

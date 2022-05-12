c4b7644e50f2598ec189fd5f32f4a762bec0e143
djsavvy <sraghuvanshi@college.harvard.edu>
2022-05-12 18:49:41 -0400

    Implement reader for our custom format

--- parser/src/custom_format.rs
+++ parser/src/custom_format.rs
@@ -1,12 +1,15 @@
-use std::fs::File;
-use std::io::BufWriter;
-
-use bincode::serialize_into;
-
 use crate::{
     output_writer::OutputWriter,
     transaction::{Block, InputOutputPair, Transaction},
 };
+use bincode::serialize_into;
+use serde::{de::DeserializeOwned, Deserialize};
+use std::io::BufWriter;
+use std::{fs::File, io::ErrorKind};
+
+const TRANSACTION_DBFILE: &'static str = "transactions.customdb";
+const BLOCK_DBFILE: &'static str = "blocks.customdb";
+const IOPAIR_DBFILE: &'static str = "iopair.customdb";
 
 pub struct CustomWriter {
     tx_writer: BufWriter<std::fs::File>,
@@ -17,9 +20,9 @@ pub struct CustomWriter {
 impl CustomWriter {
     pub fn new() -> CustomWriter {
         CustomWriter {
-            tx_writer: BufWriter::new(File::create("transactions.customdb").unwrap()),
-            block_writer: BufWriter::new(File::create("blocks.customdb").unwrap()),
-            iopair_writer: BufWriter::new(File::create("iopairs.customdb").unwrap()),
+            tx_writer: BufWriter::new(File::create(TRANSACTION_DBFILE).unwrap()),
+            block_writer: BufWriter::new(File::create(BLOCK_DBFILE).unwrap()),
+            iopair_writer: BufWriter::new(File::create(IOPAIR_DBFILE).unwrap()),
         }
     }
 }
@@ -36,4 +39,35 @@ impl OutputWriter for CustomWriter {
     fn insert_iopair(&mut self, iopair: InputOutputPair) {
         serialize_into(&mut self.iopair_writer, &iopair).unwrap();
     }
+}
+
+fn read_custom_format<T: DeserializeOwned>(custom_db_file: &str) -> Vec<T> {
+    let data = std::fs::read(custom_db_file).unwrap();
+    let mut cursor = &data.as_slice()[..];
+    let mut vec: Vec<T> = Vec::new();
+
+    loop {
+        match bincode::deserialize_from(&mut cursor) {
+            Err(e) => match *e {
+                bincode::ErrorKind::Io(e) => match e.kind() {
+                    std::io::ErrorKind::UnexpectedEof => break,
+                    _ => panic!("{}", e),
+                },
+                f => panic!("{}", f),
+            },
+            Ok(t) => {
+                vec.push(t);
+            }
+        };
+    }
+
+    vec
+}
+
+pub fn read_custom_formats() -> (Vec<Transaction>, Vec<Block>, Vec<InputOutputPair>) {
+    let txs: Vec<Transaction> = read_custom_format(TRANSACTION_DBFILE);
+    let blocks: Vec<Block> = read_custom_format(BLOCK_DBFILE);
+    let iopairs: Vec<InputOutputPair> = read_custom_format(IOPAIR_DBFILE);
+
+    (txs, blocks, iopairs)
 }

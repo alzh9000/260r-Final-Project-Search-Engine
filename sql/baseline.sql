3f2f628c1dad5760c6accd5c97c1963b53658ede
Albert Zhang <albertzhang9000@gmail.com>
2022-05-12 17:30:20 -0400

    try to find parent transactions

--- sql/baseline.sql
+++ sql/baseline.sql
@@ -1,16 +1,17 @@
 .timer ON
 
-SELECT *
-FROM blocks
-LIMIT 5;
-SELECT *
-FROM transactions
-LIMIT 5;
-SELECT *
-FROM input_output_pairs
-LIMIT 5;
+-- SELECT *
+-- FROM blocks
+-- LIMIT 5000;
+-- SELECT *
+-- FROM transactions
+-- LIMIT 5;
+-- SELECT *
+-- FROM input_output_pairs
+-- LIMIT 5;
 
-EXPLAIN QUERY PLAN
+-- EXPLAIN QUERY PLAN
 SELECT *
 FROM input_output_pairs
+WHERE input_output_pairs.value = 3342000000
 LIMIT 5;

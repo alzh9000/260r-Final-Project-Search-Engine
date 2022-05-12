8f2f3e814155a1a568c1318f4ea72e159e821075
djsavvy <sraghuvanshi@college.harvard.edu>
2022-05-11 22:16:04 -0400

    Set range to match max range without crashing (currently)

--- parser/src/parser.rs
+++ parser/src/parser.rs
@@ -81,7 +81,7 @@ impl Parser {
     pub fn parse(&mut self) {
         let mut files: Vec<String> = vec![];
 
-        for i in 0..100 {
+        for i in 1..373 {
             files.push(format!(
                 "/Volumes/SavvyT7Red/BitcoinCore/blocks/blk{:05}.dat",
                 i

cc32a9693621c304531b107ed1dfa6339d3d193f
djsavvy <sraghuvanshi@college.harvard.edu>
2022-05-13 22:07:26 -0400

    Implement parser main() with args parsing

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,17 +1,44 @@
-use parser::custom_format::{read_custom_formats, sort_data, CustomWriter};
+use clap::{ArgEnum, Parser};
+use parser::custom_format::{sort_data, CustomWriter};
+
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Args {
+    #[clap(arg_enum, short, long, default_value = "Operation::DumpSortedCustomDB")]
+    operation: Operation,
+
+    #[clap(short, long, default_value = "1")]
+    dat_files_to_parse: u32,
+}
+
+#[derive(Clone, ArgEnum, Debug)]
+enum Operation {
+    DumpSqlite,
+    DumpUnsortedCustomDB,
+    DumpSortedCustomDB,
+}
 
 fn main() {
     println!("Hello, world!");
+    let args = Args::parse();
 
-    // Change this type to write output in a different format (e.g. SQLiteDriver, or CustomWriter)
-    let mut custom_drainer = CustomWriter::new();
-
-    // let sqlite_connection = rusqlite::Connection::open("btc-test.db").unwrap();
-    // let mut sqlite_drainer = sqlite::SQLiteDriver::new(&sqlite_connection);
-
-    let mut p = parser::parser::Parser::new(&mut custom_drainer);
-
-    p.parse(1);
-
-    sort_data();
+    match args.operation {
+        Operation::DumpSqlite => {
+            let sqlite_connection = rusqlite::Connection::open("btc-test.db").unwrap();
+            let mut sqlite_drainer = parser::sqlite::SQLiteDriver::new(&sqlite_connection);
+            let mut p = parser::parser::Parser::new(&mut sqlite_drainer);
+            p.parse(args.dat_files_to_parse);
+        }
+        Operation::DumpUnsortedCustomDB => {
+            let mut custom_drainer = CustomWriter::new();
+            let mut p = parser::parser::Parser::new(&mut custom_drainer);
+            p.parse(args.dat_files_to_parse);
+        }
+        Operation::DumpSortedCustomDB => {
+            let mut custom_drainer = CustomWriter::new();
+            let mut p = parser::parser::Parser::new(&mut custom_drainer);
+            p.parse(args.dat_files_to_parse);
+            sort_data();
+        }
+    }
 }

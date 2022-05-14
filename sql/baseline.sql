36cb6d30c09592c683af053d2d2d3926280caab9
Albert Zhang <albertzhang9000@gmail.com>
2022-05-13 21:16:20 -0400

    update sql to get table info

--- sql/baseline.sql
+++ sql/baseline.sql
@@ -1,14 +1,19 @@
 .timer ON
 
-SELECT *
-FROM blocks
-LIMIT 5;
-SELECT *
-FROM transactions
-LIMIT 5;
-SELECT *
-FROM input_output_pairs
-LIMIT 5;
+
+
+-- Outdated, ignore:
+
+-- Getting the column names/schemas of the tables
+-- SELECT *
+-- FROM blocks
+-- LIMIT 5;
+-- SELECT *
+-- FROM transactions
+-- LIMIT 5;
+-- SELECT *
+-- FROM input_output_pairs
+-- LIMIT 5;
 
 -- EXPLAIN QUERY PLAN
 

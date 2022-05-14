662f8c349eb08a5bc3dcb9e60f62e4965231e942
Albert Zhang <albertzhang9000@gmail.com>
2022-05-13 20:30:46 -0400

    updated sql to get column info

--- sql/baseline.sql
+++ sql/baseline.sql
@@ -1,21 +1,21 @@
 .timer ON
 
--- SELECT *
--- FROM blocks
--- LIMIT 5000;
--- SELECT *
--- FROM transactions
--- LIMIT 5;
--- SELECT *
--- FROM input_output_pairs
--- LIMIT 5;
+SELECT *
+FROM blocks
+LIMIT 5;
+SELECT *
+FROM transactions
+LIMIT 5;
+SELECT *
+FROM input_output_pairs
+LIMIT 5;
 
 -- EXPLAIN QUERY PLAN
 
-SELECT *
-FROM input_output_pairs
-WHERE input_output_pairs.src_tx='��v�y��|�WX�bR_��zh2�b	]T�c�tsC'
-LIMIT 500;
+-- SELECT *
+-- FROM input_output_pairs
+-- WHERE input_output_pairs.src_tx='��v�y��|�WX�bR_��zh2�b	]T�c�tsC'
+-- LIMIT 500;
 
 -- -- To find the parent transactions of X
 -- SELECT *
